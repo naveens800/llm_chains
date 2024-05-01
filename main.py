bba655be1f5660e1318fa516d5a424b005bbdbeb
Naveen Sargam <naveensargam@gmail.com>
2024-05-01 18:19:23 +0530

    used output parser to get the desired response

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from langchain import PromptTemplate, OpenAI, LLMChain
+from langchain.output_parsers import CommaSeparatedListOutputParser
 from langchain.chat_models import ChatOpenAI
 
 prompt_template = "What is a word to replace the following: {word}?"
@@ -6,20 +7,12 @@ prompt_template = "What is a word to replace the following: {word}?"
 # Set the "OPENAI_API_KEY" environment variable before running following line.
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
+output_parser = CommaSeparatedListOutputParser()
+template = """List all possible words as substitute for 'artificial' as comma separated."""
+
 llm_chain = LLMChain(
     llm=llm,
-    prompt=PromptTemplate.from_template(prompt_template)
-)
+    prompt=PromptTemplate(template=template, output_parser=output_parser, input_variables=[]),
+    output_parser=output_parser)
 
-print(f'{llm_chain("artificial")=}\n\n')
-input_list = [
-    {"word": "artificial"},
-    {"word": "intelligence"},
-    {"word": "robot"}
-]
-
-print(f'{llm_chain.apply(input_list) = }\n\n')
-
-print(f'{llm_chain.generate(input_list) = }\n\n')
-
-print(f'{llm_chain.predict(word="robot") = }')
+print(f'{llm_chain.predict() = }')
